3551b7f321cb13a0cd2770030b4b5876e6d64f60
mixdone <m.diza@mail.ru>
2023-09-20 16:45:29 +0700

    task-1-1-1

---
+++ task-1-1-1/src/test/java/HeapSortTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapSortTestTest {
+
+    @Test
+    void checkHeapsort() {
+        assertArrayEquals(HeapSort.heapsort(new int[] {5, 4, 3, 2, 1}), new int[] {1, 2, 3, 4, 5});
+
+        assertArrayEquals(HeapSort.heapsort(new int[] {-1, 2, 1000000, 1, 1, 2, 3}), new int[] {-1, 1, 1, 2, 2, 3, 1000000});
+
+        assertArrayEquals(HeapSort.heapsort(new int[] {-2, -1, 0, 1, 2, 3, 4}), new int[] {-2, -1, 0, 1, 2, 3, 4});
+    }
+
+}
