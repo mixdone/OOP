9b058335dd13706c97801bb8e447613430f72469
mixdone <m.diza@mail.ru>
2023-09-21 10:13:24 +0700

    ParametrizedTest added

--- task-1-1-1/src/test/java/HeapSortTest.java
+++ task-1-1-1/src/test/java/HeapSortTest.java
@@ -1,16 +1,26 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class HeapSortTestTest {
 
-    @Test
-    void checkHeapsort() {
-        assertArrayEquals(HeapSort.heapsort(new int[] {5, 4, 3, 2, 1}), new int[] {1, 2, 3, 4, 5});
-
-        assertArrayEquals(HeapSort.heapsort(new int[] {-1, 2, 1000000, 1, 1, 2, 3}), new int[] {-1, 1, 1, 2, 2, 3, 1000000});
-
-        assertArrayEquals(HeapSort.heapsort(new int[] {-2, -1, 0, 1, 2, 3, 4}), new int[] {-2, -1, 0, 1, 2, 3, 4});
+    @ParameterizedTest
+    @MethodSource("getTestArg")
+    void checkHeapsort(int[] input, int[] expected) {
+        assertArrayEquals(HeapSort.heapsort(input), expected);
     }
 
+
+    static Stream<Arguments> getTestArg() {
+        return Stream.of(
+                Arguments.of(new int[] {5, 4, 3, 2, 1}, new int[] {1, 2, 3, 4, 5}),
+                Arguments.of(new int[] {-1, 2, 1000000, 1, 1, 2, 3}, new int[] {-1, 1, 1, 2, 2, 3, 1000000}),
+                Arguments.of(new int[] {-2, -1, 0, 1, 2, 3, 4}, new int[] {-2, -1, 0, 1, 2, 3, 4}),
+                Arguments.of(new int[] {}, new int[] {}),
+                Arguments.of(new int[] {1}, new int[] {1}));
+    }
 }
