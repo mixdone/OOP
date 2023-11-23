0c7a641f33a37617c65210e9dc0904132065cb09
Michael Diza <m.diza@g.nsu.ru>
2023-11-23 10:31:37 +0700

    change indentation level

--- Task_1_4_1/src/main/java/CreditBook.java
+++ Task_1_4_1/src/main/java/CreditBook.java
@@ -10,7 +10,7 @@ public class CreditBook {
     void addSemester(Semester semester) {
        this.semesters.add(semester);
     }
-   void setGraduationWork(SubjectWithMark work) {
+    void setGraduationWork(SubjectWithMark work) {
        this.graduationWork = work;
    }
     /**
@@ -18,43 +18,43 @@ public class CreditBook {
      *
      * @return средняя арифметическая оценка.
      */
-   public double getAverageMark() {
-       double summ = semesters.stream().mapToDouble(x ->
-               x.getSubjects().stream().mapToInt(y ->
-                       y.getMark().value).sum()).sum();
-       double count = semesters.stream().mapToDouble(x ->
-               x.getSubjects().size()).sum();
-       return summ / count;
-   }
+    public double getAverageMark() {
+        double summ = semesters.stream().mapToDouble(x ->
+                x.getSubjects().stream().mapToInt(y ->
+                        y.getMark().value).sum()).sum();
+        double count = semesters.stream().mapToDouble(x ->
+                x.getSubjects().size()).sum();
+        return summ / count;
+    }
 
     /**
      * Возможность получения красного диплома.
      *
      * @return true/false.
      */
-   public boolean honoursDegree() {
-       boolean notBadMark = semesters.stream().allMatch(x ->
-               x.getSubjects().stream().allMatch(y ->
-                       y.getMark().value > 3));
-       double fivesAmount = semesters.stream().mapToLong(x ->
-               x.getSubjects().stream().filter(y ->
-                       y.getMark().value == 5).count()).sum();
-       double marksAmount = semesters.stream().mapToLong(x ->
-               x.getSubjects().size()).sum();
+    public boolean honoursDegree() {
+        boolean notBadMark = semesters.stream().allMatch(x ->
+                x.getSubjects().stream().allMatch(y ->
+                        y.getMark().value > 3));
+        double fivesAmount = semesters.stream().mapToLong(x ->
+                x.getSubjects().stream().filter(y ->
+                        y.getMark().value == 5).count()).sum();
+        double marksAmount = semesters.stream().mapToLong(x ->
+                   x.getSubjects().size()).sum();
 
-       return (fivesAmount / marksAmount > 0.75)
-               && notBadMark
-               && (graduationWork.getMark().value == 5);
-   }
+        return (fivesAmount / marksAmount > 0.75)
+                && notBadMark
+                && (graduationWork.getMark().value == 5);
+    }
 
     /**
      * Будет ли стипендия повышенной.
      *
      * @return true/false.
      */
-   public boolean increasedStipend() {
-       return semesters.get(semesters.size() - 1).getSubjects().stream().allMatch(x ->
-               x.getMark().value == 5);
-   }
+    public boolean increasedStipend() {
+        return semesters.get(semesters.size() - 1).getSubjects().stream().allMatch(x ->
+                x.getMark().value == 5);
+    }
 
 }
