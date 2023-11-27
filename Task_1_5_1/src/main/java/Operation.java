07b9898c2f12efcd3b087e8ed18b650ffb998f03
Michael Diza <m.diza@g.nsu.ru>
2023-11-27 22:21:57 +0700

    change switch type

--- Task_1_5_1/src/main/java/Operation.java
+++ Task_1_5_1/src/main/java/Operation.java
@@ -62,21 +62,37 @@ public class Operation {
         double answer;
         if (argumentCount == 1) {
             double arg = stack.pop();
-            answer = switch (name) {
-                case "sin"  -> Math.sin(arg);
-                case "cos"  -> Math.cos(arg);
-                case "sqrt" -> Math.sqrt(arg);
-                case "log"  -> Math.log(arg);
-                default     -> NaN;
+            switch (name) {
+                case "sin":
+                    answer = Math.sin(arg);
+                    break;
+                case "cos":
+                    answer = Math.cos(arg);
+                    break;
+                case "sqrt":
+                    answer = Math.sqrt(arg);
+                    break;
+                case "log":
+                    answer = Math.log(arg);
+                    break;
+                default:
+                    answer = NaN;
             };
         } else if (!name.equals("-")){
             double arg1 = stack.pop();
             double arg2 = stack.pop();
-            answer = switch (name) {
-                case "+"    -> arg1 + arg2;
-                case "*"    -> arg1 * arg2;
-                case "pow"  -> Math.pow(arg1, arg2);
-                default     -> NaN;
+            switch (name) {
+                case "+":
+                    answer = arg1 + arg2;
+                    break;
+                case "*":
+                    answer = arg1 * arg2;
+                    break;
+                case "pow":
+                    answer = Math.pow(arg1, arg2);
+                    break;
+                default:
+                    answer = NaN;
             };
         } else {
             double arg1 = stack.pop();
