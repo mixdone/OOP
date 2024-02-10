595955b4469c16fc60a2ea37faa2f8e4cdb839bb
mixdone <mixdone@users.noreply.github.com>
2024-02-10 10:45:01 +0000

    Deploying to gh-pages from @ mixdone/OOP@d87729737f8c7a4670098ac0487b2d21b7e86486 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"IsNotPrime"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"SequentialSearch"},{"p":"<Unnamed>","l":"StreamSearch"},{"p":"<Unnamed>","l":"ThreadSearch"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"IsNotPrime"},{"p":"<Unnamed>","l":"SequentialSearch"},{"p":"<Unnamed>","l":"StreamSearch"},{"p":"<Unnamed>","l":"ThreadSearch"}]
