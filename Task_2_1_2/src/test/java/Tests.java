980753abc982ecf9e7c5b21f5299536b083f57a2
Michael Diza <m.diza@g.nsu.ru>
2024-02-11 20:55:28 +0700

    increase coverage

--- Task_2_1_2/src/test/java/Tests.java
+++ Task_2_1_2/src/test/java/Tests.java
@@ -1,12 +1,37 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Tests {
+
+    static class MyThread extends Thread {
+        private int num;
+
+        public MyThread(int n) {
+            num = n;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                if (num == 0) {
+                    Server.main("");
+                } else {
+                    Client.main("");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        }
+    }
+
     @Test
     public void test() throws IOException {
-        Server.main("server");
-        Client.main("client");
-        Client.main("client");
+        ArrayList<MyThread> threads = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            threads.add(new MyThread(i));
+        }
     }
 }
