6ef52d2866e412a0537e9c17ffbc07c3e97c2ec0
Michael Diza <m.diza@g.nsu.ru>
2024-03-13 11:02:18 +0700

    add tests

---
+++ Task_2_1_2/src/test/java/TestServerWithoutClient.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestServerWithoutClient {
+
+    Boolean result = false;
+
+    private static class ClientThread implements Runnable {
+
+        @Override
+        public void run() {
+            Client.main(new String[]{""});
+        }
+    }
+
+    private class ServerThread implements Runnable {
+        Server server;
+        ArrayList<Integer> list;
+
+        public ServerThread(Server server, ArrayList<Integer> list) {
+            this.server = server;
+            this.list   = list;
+        }
+
+        @Override
+        public void run() {
+            result = server.start(list);
+        }
+    }
+
+    @Test
+    public void testFalse() {
+        var list = new ArrayList<>(Arrays.asList(3, 5, 7, 11, 13, 17));
+        var server  = new Thread(new TestServerWithoutClient.ServerThread(new Server(), list));
+
+        server.start();
+
+        try {
+            server.join();
+            Assertions.assertFalse(result);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
