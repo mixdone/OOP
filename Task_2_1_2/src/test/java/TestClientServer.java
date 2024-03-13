1660054a44a718b2e36fc86f13763577a6943bb3
Michael Diza <m.diza@g.nsu.ru>
2024-03-13 11:44:50 +0700

    expand tests

--- Task_2_1_2/src/test/java/TestClientServer.java
+++ Task_2_1_2/src/test/java/TestClientServer.java
@@ -34,18 +34,24 @@ public class TestClientServer {
 
     @Test
     public void testTrue() {
-        var list = new ArrayList<>(Arrays.asList(3, 5, 7, 11, 12, 13, 21, 17, 1, 0, 2));
+        var list = new ArrayList<>(Arrays.asList(3, 0, 5, 7, 11, 12, 13, 21, 17, 23, 19, 1, 3, 7, 5, 2));
         var server  = new Thread(new ServerThread(new Server(), list));
         var c1      = new Thread(new ClientThread());
         var c2      = new Thread(new ClientThread());
+        var c3      = new Thread(new ClientThread());
+        var c4      = new Thread(new ClientThread());
 
         server.start();
         c1.start();
         c2.start();
+        c3.start();
+        c4.start();
 
         try {
             c1.join();
             c2.join();
+            c3.join();
+            c4.join();
             server.join();
 
             Assertions.assertTrue(result);
