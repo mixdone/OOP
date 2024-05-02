6e982c10ea7870efbf2369abf188a4b25f2eb285
Michael Diza <m.diza@g.nsu.ru>
2024-05-02 10:36:48 +0700

    grable main class changes

--- Task_2_3_1/src/main/java/game/task/HelloApplication.java
+++ Task_2_3_1/src/main/java/game/task/HelloApplication.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 /**
  * Main class.
  */
-public class Main extends Application {
+public class HelloApplication extends Application {
 
     /**
      * Start.
@@ -22,7 +22,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("start.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Snake Game");
         stage.setScene(scene);
